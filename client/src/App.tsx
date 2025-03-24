303759106dd9a745a5d6aeaf0a3f742a70b0fa1b
Josh <jcpm2603@gmail.com>
2025-03-24 01:07:23 -0400

    pages set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { Route, createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider
+} from 'react-router-dom';
+
 import './App.css'
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} ></Route>
+    </Route>
+
+  )
+);
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ client/src/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from '../pages/HomePage'
+
+
+
+const MainLayout = () => {
+  return (
+    <HomePage/>
+  )
+}
+
+export default MainLayout
